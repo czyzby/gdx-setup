ca38ee11b83f9ef2eb7559edd010be4b4c641be0
Paweł Pastuszak <kotcrab@gmail.com>
2016-05-07 23:45:52 +0200

    Restart with XstartOnFirstThread when it was not used, close #16

--- src/main/kotlin/com/github/czyzby/setup/main.kt
+++ src/main/kotlin/com/github/czyzby/setup/main.kt
@@ -10,6 +10,7 @@ import com.github.czyzby.setup.views.Extension
 import com.github.czyzby.setup.views.GdxPlatform
 import com.github.czyzby.setup.views.JvmLanguage
 import com.github.czyzby.setup.views.ProjectTemplate
+import com.kotcrab.vis.ui.util.OsUtils
 
 fun main(args: Array<String>) {
     val config = Lwjgl3ApplicationConfiguration()
@@ -18,13 +19,26 @@ fun main(args: Array<String>) {
     config.disableAudio(true)
     config.setDecorated(false)
     // arrayOf(256, 128, 64, 32, 16).forEach { config.addIcon("icons/libgdx$it.png", Files.FileType.Classpath) }
-    Lwjgl3Application(object : AutumnApplication(DesktopClassScanner(), Root::class.java) {
-        override fun registerDefaultComponentAnnotations(initializer: ContextInitializer) {
-            super.registerDefaultComponentAnnotations(initializer)
-            initializer.scanFor(Extension::class.java, ProjectTemplate::class.java, JvmLanguage::class.java,
-                    GdxPlatform::class.java)
+
+    try {
+        Lwjgl3Application(object : AutumnApplication(DesktopClassScanner(), Root::class.java) {
+            override fun registerDefaultComponentAnnotations(initializer: ContextInitializer) {
+                super.registerDefaultComponentAnnotations(initializer)
+                initializer.scanFor(Extension::class.java, ProjectTemplate::class.java, JvmLanguage::class.java,
+                        GdxPlatform::class.java)
+            }
+        }, config)
+    } catch(error: ExceptionInInitializerError) {
+        if (OsUtils.isMac() && error.cause is IllegalStateException) {
+            if (error.stackTraceToString().contains("Please run the JVM with -XstartOnFirstThread.")) {
+                System.out.println("Application was not launched on first thread. Restarting with -XstartOnFirstThread." +
+                        "Add VM argument -XstartOnFirstThread to avoid this.");
+                Application.startNewInstance();
+            }
         }
-    }, config)
+
+        throw error;
+    }
 }
 
 /**
