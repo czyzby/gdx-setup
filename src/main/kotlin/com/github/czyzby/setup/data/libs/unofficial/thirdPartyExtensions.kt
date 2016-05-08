c0fa3479bf540c304be86513c8034d004460e3a6
MJ <john.hervicc@gmail.com>
2016-05-08 12:15:25 +0200

    Added SquidLib libraries to third-party dependencies. Close #10

--- src/main/kotlin/com/github/czyzby/setup/data/libs/unofficial/thirdPartyExtensions.kt
+++ src/main/kotlin/com/github/czyzby/setup/data/libs/unofficial/thirdPartyExtensions.kt
@@ -405,3 +405,72 @@ class WebsocketSerialization : ThirdPartyExtension() {
         Websocket().initiate(project)
     }
 }
+
+/**
+ * Version of SquidLib libraries.
+ * @author SquidPony
+ */
+const val SQUID_LIB_VERSION = "3.0.0-b5"
+/**
+ * URL of SquidLib libraries.
+ * @author SquidPony
+ */
+const val SQUID_LIB_URL = "https://github.com/SquidPony/SquidLib"
+
+/**
+ * Cross-platform regex utilities.
+ * @author Tommy Ettinger
+ */
+@Extension
+class RegExodus : ThirdPartyExtension() {
+    override val id = "regExodus"
+    override val defaultVersion = "0.1.3"
+    override val url = "https://github.com/tommyettinger/RegExodus"
+
+    override fun initiateDependencies(project: Project) {
+        addDependency(project, Core.ID, "com.github.tommyettinger:regexodus")
+
+        addDependency(project, GWT.ID, "com.github.tommyettinger:regexodus:sources")
+        addGwtInherit(project, "regexodus")
+    }
+}
+
+/**
+ * Utility for grid-based games.
+ * @author SquidPony
+ */
+@Extension
+class SquidLibUtil : ThirdPartyExtension() {
+    override val id = "squidLibUtil"
+    override val defaultVersion = SQUID_LIB_VERSION
+    override val url = SQUID_LIB_URL
+
+    override fun initiateDependencies(project: Project) {
+        addDependency(project, Core.ID, "com.squidpony:squidlib-util")
+
+        addDependency(project, GWT.ID, "com.squidpony:squidlib-util:sources")
+        addGwtInherit(project, "squidlib-util")
+
+        RegExodus().initiate(project)
+    }
+}
+
+/**
+ * Utility for roguelike games.
+ * @author SquidPony
+ */
+@Extension
+class SquidLib : ThirdPartyExtension() {
+    override val id = "squidLib"
+    override val defaultVersion = SQUID_LIB_VERSION
+    override val url = SQUID_LIB_URL
+
+    override fun initiateDependencies(project: Project) {
+        addDependency(project, Core.ID, "com.squidpony:squidlib")
+
+        addDependency(project, GWT.ID, "com.squidpony:squidlib:sources")
+        addGwtInherit(project, "squidlib")
+
+        RegExodus().initiate(project)
+    }
+}
