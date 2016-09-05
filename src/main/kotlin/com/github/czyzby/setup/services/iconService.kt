46c815f446272dae4aedc4744f1c84aae13943d1
Paweł Pastuszak <kotcrab@gmail.com>
2016-09-05 20:55:10 +0200

    Refactored icon setting to use glfwSetWindowIcon, removed JNA

--- src/main/kotlin/com/github/czyzby/setup/services/iconService.kt
+++ src/main/kotlin/com/github/czyzby/setup/services/iconService.kt
@@ -1,12 +1,15 @@
 package com.github.czyzby.setup.services
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Pixmap
 import com.github.czyzby.autumn.annotation.Component
 import com.github.czyzby.autumn.annotation.Initiate
-import com.github.czyzby.setup.util.GLFWIconSetter
+import com.kotcrab.vis.ui.util.OsUtils
+import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFWImage
 
 /**
- * Invokes [GLFWIconSetter] to change application icon after startup.
+ * Changes application icon after startup.
  * @author Kotcrab
  */
 @Component
@@ -14,8 +17,27 @@ class IconService {
     @Initiate
     fun setApplicationIcon() {
         try {
-            GLFWIconSetter.newInstance().setIcon(Gdx.files.internal("icons/libgdx128.ico"),
-                    Gdx.files.internal("icons/libgdx128.png"));
+            if (!OsUtils.isMac()) {
+                val iconPaths = arrayOf("icons/libgdx16.png", "icons/libgdx32.png", "icons/libgdx64.png",
+                        "icons/libgdx128.png")
+                val pixmaps = iconPaths.map { Pixmap(Gdx.files.internal(it)) }
+
+                val buffer = GLFWImage.malloc(iconPaths.size)
+                pixmaps.forEach { pixmap ->
+                    if (pixmap.format != Pixmap.Format.RGBA8888) {
+                        throw IllegalStateException("Incorrect icon pixmap format, must be RGBA8888.")
+                    }
+                    val icon = GLFWImage.malloc().set(pixmap.width, pixmap.height, pixmap.pixels)
+                    buffer.put(icon)
+                    icon.free()
+                }
+
+                buffer.position(0)
+                GLFW.glfwSetWindowIcon(GLFW.glfwGetCurrentContext(), buffer)
+                buffer.free()
+
+                pixmaps.forEach(Pixmap::dispose)
+            }
         } catch(throwable: Throwable) {
             throwable.printStackTrace()
         }
