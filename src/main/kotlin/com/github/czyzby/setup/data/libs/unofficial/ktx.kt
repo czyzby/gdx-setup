45e78145a5f90593c2225cf2b1e6666cc09cedc2
MJ <john.hervicc@gmail.com>
2016-10-29 12:59:24 +0200

    Updated KTX libraries version.

--- src/main/kotlin/com/github/czyzby/setup/data/libs/unofficial/ktx.kt
+++ src/main/kotlin/com/github/czyzby/setup/data/libs/unofficial/ktx.kt
@@ -9,7 +9,7 @@ import com.github.czyzby.setup.views.Extension
  * Current version of KTX libraries.
  * @author MJ
  */
-const val KTX_VERSION = "1.9.4-b1"
+const val KTX_VERSION = "1.9.4-b2"
 
 /**
  * Kotlin utilities for Scene2D actors API.
